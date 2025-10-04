d98c95b87f94a69c021d3229583e0c30fe244d97
MarcospAntunes <marcospantunes203@gmail.com>
2025-10-04 07:07:55 -0300

    Update index.ts

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,6 @@
 async function apiRanking(value: string = "0") {
   const req = await fetch(`http://localhost:3000/api/simulate-ranking?mile_value=${value}`);
   const res = await req.json();
-  console.log(res)
   return res;
 }
 
